7fe22f85b003d8a962b5f2a981b924262bb6585e
Nick Van Dyck <vandyck.nick@outlook.com>
2024-03-22 17:24:37 +0100

    feat: add python venv selector

--- init.lua
+++ init.lua
@@ -723,6 +723,23 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim' },
+    opts = {
+      -- Your options go here
+      -- name = "venv",
+      -- auto_refresh = false
+    },
+    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    },
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
