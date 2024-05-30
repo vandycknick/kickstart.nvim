73796d93d0762069696b013dc34144f0a2ad6cf0
Nick Van Dyck <vandyck.nick@outlook.com>
2024-05-30 22:41:00 +0200

    feat: remove cokeline for now

--- init.lua
+++ init.lua
@@ -1154,24 +1154,25 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'willothy/nvim-cokeline',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- Required for v0.4.0+
-      'nvim-tree/nvim-web-devicons', -- If you want devicons
-    },
-    opts = {
-      sidebar = {
-        filetype = { 'NvimTree' },
-        components = {
-          {
-            text = '',
-            bg = 'TabLineFill',
-          },
-        },
-      },
-    },
-  },
+  -- I don't think I need this anymore
+  -- {
+  --   'willothy/nvim-cokeline',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim', -- Required for v0.4.0+
+  --     'nvim-tree/nvim-web-devicons', -- If you want devicons
+  --   },
+  --   opts = {
+  --     sidebar = {
+  --       filetype = { 'NvimTree' },
+  --       components = {
+  --         {
+  --           text = '',
+  --           bg = 'TabLineFill',
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 
   -- Config for nice nvim sql integration
   { 'tpope/vim-dadbod' },
